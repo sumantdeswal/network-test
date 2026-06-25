a852c48292b0938b8303114998b87f2967ed5059
Sumant Deswal <sumantd1206@gmail.com>
2026-06-25 17:42:28 +0530

    Update

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const axios = require("axios");
 const express = require("express");
 const path = require("path");
-
+const fs = require("fs-extra");
 const app = express();
+
+const LOG_DIR = path.join(__dirname, "logs");
+
+fs.ensureDirSync(LOG_DIR);
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
@@ -15,6 +19,34 @@ app.post("/collect", async (req, res) => {
     try {
 
         const response = await axios.get(`https://ipwho.is/${ip}`);
+
+        const log = {
+
+            timestamp: new Date().toISOString(),
+                
+            request: {
+                ip,
+                method: req.method,
+                protocol: req.protocol,
+                httpVersion: req.httpVersion
+            },
+        
+            browser: req.body.browser,
+        
+            network: response.data,
+        
+            headers: req.headers
+        
+        };
+        
+        const filename = path.join(
+            LOG_DIR,
+            `${Date.now()}.json`
+        );
+        
+        await fs.writeJson(filename, log, {
+            spaces: 4
+        });
 
         res.json({
             success: true,
